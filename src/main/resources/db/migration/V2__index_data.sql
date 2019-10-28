8ddf8ddf031c13f4baeec326b2acaa8bd7b2a441
Eugen Druzin <edruzin@gmail.com>
2019-10-28 09:47:50 +0100

    Add db indexes

--- src/main/resources/db/migration/V2__index_data.sql
+++ src/main/resources/db/migration/V2__index_data.sql
@@ -1,7 +1,9 @@
 CREATE INDEX idx_app_user_email ON app_user(email);
+CREATE INDEX idx_app_user_role ON app_user(role_id);
 CREATE INDEX idx_user_info_web_session_uuid ON user_info(web_session_uuid);
 
 CREATE INDEX idx_bank_account_user_uuid ON bank_account(user_uuid);
+CREATE INDEX idx_bank_account_user_uuid_and_id ON bank_account(user_uuid, id);
 CREATE INDEX idx_forgot_password_token_token ON forgot_password_token(token);
 
 CREATE INDEX idx_mail_token_token ON mail_token(token);
